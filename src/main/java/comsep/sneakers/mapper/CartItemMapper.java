31f68651a949a918a5cfb206a712cec517832016
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2025-03-20 21:46:58 +0600

    Sorry, Agay, I forgot to comment on my project before.

--- src/main/java/comsep/sneakers/mapper/CartItemMapper.java
+++ src/main/java/comsep/sneakers/mapper/CartItemMapper.java
@@ -9,6 +9,7 @@ import org.mapstruct.factory.Mappers;
 public interface CartItemMapper {
     CartItemMapper INSTANCE = Mappers.getMapper(CartItemMapper.class);
 
+
     CartItemDTO toDto(CartItem cartItem);
     CartItem toEntity(CartItemDTO cartItemDTO);
 }
